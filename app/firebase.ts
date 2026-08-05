7762cc814681d8534658cfc10b0312b41a760cc9
onion291 <0zero0.778.0@gmail.com>
2026-08-05 10:22:28 +0900

    Store profile photos in Firebase Storage

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCOv_VYHIpP9tBjrGfyoCFriOslNUId22Q",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 export const firebaseApp = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseApp);
 export const firebaseDb = getFirestore(firebaseApp);
+export const firebaseStorage = getStorage(firebaseApp);
